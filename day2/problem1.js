c9c0a14e4df0298f64862ba0f48e5398a802ebfc
Khushboo Sharma <khushi.3sharma.3.2@gmail.com>
2023-01-31 08:16:32 +0400

    day 2 programs adde

---
+++ day2/problem1.js
@@ -0,0 +1,91 @@
+
+// console.log(5 == 6);
+// console.log("5" === 5);
+// console.log("khushboo" === (Khushboo));
+
+// let item_price = 1000;
+// let money_you_have = 500;
+
+// if (1000 < 500) {
+//      console.log("i bought item ");
+// } else {
+//      console.log("i dont have money ");
+// };
+
+
+// const toothpast = "dbar";
+
+// if (toothpast === "colget") {
+//      console.log("i get it");
+// } else if (toothpast === "dabar") {
+//      console.log("i get it");
+// } else if (toothpast === "Daberred thothpast") {
+//      console.log("i get it");
+// } else {
+//      console.log("i dint get ");
+// }
+
+
+
+// const num1 = 150;
+// const num2 = 165;
+
+// if (num1 > num2) {
+//      console.log("num1 is grater");
+
+// } else if (num1 < num2) {
+//      console.log("num2 is grator");
+
+// } else if (num1 === num2) {
+//      console.log("equal");
+// };
+
+
+
+
+
+
+// let roadSiganal = "yello";
+
+// if (roadSiganal == "red") {
+//      console.log("stop!");
+
+// } else if (roadSiganal == "green") {
+//      console.log("go");
+
+// } else if (roadSiganal == "yello") {
+//      console.log("ready for go");
+
+// } else {
+//      console.log("error");
+// };
+
+
+
+
+
+
+
+// const age = 17;
+
+// if (age >= 18) {
+//      console.log("Apply for license")
+// } else {
+//      console.log("NA")
+// }
+
+
+
+// let electronic_stor = "redmi";
+
+// if (electronic_stor === "redmi") {
+//      console.log("yes");
+// } else {
+//      console.log("no");
+// }
+
+
+// let electronic_stor = "redmi";
+// condition ? true result: false result.
+
+// (electronic_stor === "redmi") ? console.log("i will buy mobile") : console.log("i will not bye mobile")

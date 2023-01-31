7ab8c70a3affde9f535c0fb4a6ec7d45bb132d21
Khushboo Sharma <khushi.3sharma.3.2@gmail.com>
2023-01-31 08:14:42 +0400

    day 1 programs added

---
+++ day1/index.js
@@ -0,0 +1,33 @@
+
+
+// let num = 2;
+// let num1 = 5;
+
+// console.log((num+num1) + (num - num1) + (num / num1)+ )
+
+
+// let sum = 3;
+// let sum1 = 2;
+
+
+// console.log("add is", sum + sum1)
+
+// console.log(sum - sum1)
+
+// console.log(sum * sum1)
+
+// console.log(sum / sum1)
+
+// console.log(sum % sum1)
+
+
+// let a = 5;
+// let b = 5;
+
+// console.log(a * * b)
+
+// let n = "khushboo";
+// let num = 5;
+
+// console.log((n + num))
+// console.log( typeof(n, num));
